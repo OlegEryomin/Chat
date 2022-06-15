6772b05804a659e2f9689125237ad48009cf6f5b
Eryomin Oleg <eremin_o@bsu.edu.ru>
2022-06-15 13:18:42 +0300

    Задание к уроку 4

--- src/main/java/ru/edu/bsu/chat/ChatController.java
+++ src/main/java/ru/edu/bsu/chat/ChatController.java
@@ -7,12 +7,34 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChatController {
 
+    @FXML
+    private ListView textChat;
 
+    @FXML
+    private TextField sendText;
 
+    ObservableList<String> messages = FXCollections.observableArrayList();
+    @FXML
+    private void onSendMessage() {
+        messages.add(sendText.getText());
+        textChat.setItems(messages);
+        sendText.clear();
+
+    }
+
+    @FXML
+    public void buttonPressed(KeyEvent e)
+    {
+        if(e.getCode().toString().equals("ENTER"))
+        {
+            onSendMessage();
+        }
+    }
 }
